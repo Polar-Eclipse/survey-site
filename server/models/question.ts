e891c502e34dc3d5ac481baf9e79a59f4b89c231
wenxi0504 <wxu63@my.centennialcollege.ca>
2021-07-11 19:42:51 -0400

    add question model

--- server/models/question.ts
+++ server/models/question.ts
@@ -1,17 +1,76 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema; // Schema alias
-const questionSchema = new Schema
-({
-    YourQuestion: String, 
-    FirstChoice: String,
-    SecondChoice: String,
-    ThirdChoice: String,
-    ForthChoice: String
-},
+import { model, Model, ObjectId, Schema } from "mongoose";
 
-{
-    collection: "question"
-});
+interface Question {
+    survey: ObjectId;
+    question: string;
+    required: boolean;
+    type: "text" | "yesno" | "choice";
+    options: { value: string; text: string; }[];
+    createdAt: Date;
+    updatedAt: Date;
+}
 
-const Model = mongoose.model("question",questionSchema);
-export default Model;
+const QuestionSchema = new Schema<Question, Model<Question>, Question>(
+    {
+        survey: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "Survey", 
+        },
+        question: {
+            type: String,
+            required: true,
+        },
+        required: {
+            type: Boolean,
+            default: false,
+        },
+        type: {
+            type: String,
+            enum: ["text", "yesno", "choice"], // Only one of these values
+            default: "text",
+        },
+        options: {
+            type: [{
+                value: {
+                    type: String, 
+                    required: true,
+                },
+
+                text: {
+                    type: String,
+                    required: true,
+                },
+            }], // An array of objects of the given type
+        },
+    },
+    {
+        collection: "questions",
+        timestamps: true,
+    },
+);
+
+const Question = model("Question", QuestionSchema);
+
+export default Question;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
