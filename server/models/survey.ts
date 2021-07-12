a036a9266f7fdb17293db134026a7ded5f4acdd8
wenxi0504 <wxu63@my.centennialcollege.ca>
2021-07-12 12:00:28 -0400

    adjusted codes to meet the batabase plan

--- server/models/survey.ts
+++ server/models/survey.ts
@@ -73,7 +73,6 @@ const SurveySchema = new Schema<Survey, Model<Survey>, Survey>(
                     type: String, 
                     required: true,
                 },
-
                 text: {
                     type: String,
                     required: true,
