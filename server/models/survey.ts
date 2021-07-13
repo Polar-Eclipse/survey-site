92d597adaf692ae694d25dada04881d89b7b13e5
Sophie <wenxi1988@hotmail.com>
2021-07-13 13:51:59 -0400

    adjust survey model to multiple questions

--- server/models/survey.ts
+++ server/models/survey.ts
@@ -14,35 +14,25 @@
 
 import {model, Model, ObjectId, Schema } from "mongoose";
 
-// Create an interface which TS can rely on to give use hints of what fields can be used.
-interface Survey {
-    title: string;
-    description?: string;
-    activeFrom: Date;
-    expiresAt?: Date;
-    createdAt: Date;
-    updatedAt: Date;
-    owner: ObjectId;
-    type: "text" | "yesno" | "choice";
-    options?: QuestionOptionItem[];
-}
+ // Create an interface which TS can rely on to give use hints of what fields can be used.
+ interface Survey {
+     questions: ObjectId[]
+     activeFrom: Date;
+     expiresAt?: Date;
+     createdAt: Date;
+     updatedAt: Date;
+     owner: ObjectId;
+     type: "yesno" ;
+ }
 
-export interface QuestionOptionItem {
-    value: string;
-    text: string;
-}
 
 // Note the type signature of the schema.
 const SurveySchema = new Schema<Survey, Model<Survey>, Survey>(
     {
-        title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
+        questions: [{
+            type: Schema.Types.ObjectId, // An array of ObjectId's
+            ref:"Question",
+        }],
         activeFrom: {
             type: Date,
             default: () => new Date(),
@@ -55,24 +45,11 @@ const SurveySchema = new Schema<Survey, Model<Survey>, Survey>(
         owner:{
             type: Schema.Types.ObjectId,
             required: true,
-            ref: "Owner",
+            ref: "User",
         },
         type: {
             type: String,
-            enum: ["text", "yesno", "choice"], // Only one of these values
-            default: "text",
-        },
-        options: {
-            type: [{
-                value: {
-                    type: String,
-                    required: true,
-                },
-                text: {
-                    type: String,
-                    required: true,
-                }
-            }]
+            default: "yesno",
         },
     },
     {
