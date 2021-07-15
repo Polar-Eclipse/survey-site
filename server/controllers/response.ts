0bad01393fa8a2c40ff0fa517c2d336faab8c1d4
Jamee Kim <jameekim@nodaplife.me>
2021-07-15 16:48:56 -0400

    Add external documentation to Response controller file

--- server/controllers/response.ts
+++ server/controllers/response.ts
@@ -1,5 +1,25 @@
+/**
+ * server/controllers/response.ts
+ *
+ * Controller functions for the Response database model
+ *
+ * Polar Survey
+ * @author Aun Raza (301074590)
+ * @author Jamee Kim (301058465)
+ * @author Jerome Ching (300817930)
+ * @author Sophie Xu (3010981127)
+ * @author Tien Sang Nguyen (301028223)
+ * @author Eunju Jo (301170731)
+ */
+
 import Response from "../models/reponse";
 
+/**
+ * Get an array of user reponse to the survey with the given id
+ *
+ * @param surveyId The ID of the survey
+ * @param done The callback function
+ */
 export function getAllResponse(surveyId: string, done: (err: any, res?: Response[]) => void): void
 {
     Response.find({ question: surveyId }, (err, result) =>
