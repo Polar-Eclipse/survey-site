e8763aed13ed413776a75681d34ff214d11a325d
Sophie <wenxi1988@hotmail.com>
2021-07-28 20:40:11 -0400

    Auto stash before rebase of "refs/heads/survey_userId"
    Add user's id in its owner field when a new survey is created

--- server/controllers/survey.ts
+++ server/controllers/survey.ts
@@ -77,7 +77,12 @@ export function displayEditSurveyPage(req:Request, res: Response, next: NextFunc
  * Process a request to create a survey
  */
 export function processMakeSurveyPage(req:Request, res: Response, next: NextFunction):void
-{
+{    // if req.user is undefined,then throw error
+    if (!req.user) {
+        throw Error("Unreachable: this route handler is called only when the user is logged in");
+    }
+    // pass the user _id to variable userId
+    const userId = req.user._id;
     const newSurvey = new Survey
     ({
         "questions": [
@@ -89,7 +94,8 @@ export function processMakeSurveyPage(req:Request, res: Response, next: NextFunc
         ],
         "title": req.body.title,
         "activeFrom": req.body.activeFrom,
-        "expiresAt": req.body.expiresAt || undefined
+        "expiresAt": req.body.expiresAt || undefined,
+        "owner":userId
     });
     //insert newSurvey to db
     Survey.create(newSurvey, (err) => {
