b66104fe82baa84ed62a9855650e956066524d13
Jamee Kim <jameekim@nodaplife.me>
2021-07-30 10:52:33 -0400

    Add missing headers (internal doc)

--- server/util/index.ts
+++ server/util/index.ts
@@ -1,3 +1,17 @@
+/**
+ * server/util/index.ts
+ *
+ * Utility functions used throughout the app
+ *
+ * Polar Survey
+ * @author Aun Raza (301074590)
+ * @author Jamee Kim (301058465)
+ * @author Jerome Ching (300817930)
+ * @author Sophie Xu (301098127)
+ * @author Tien Sang Nguyen (301028223)
+ * @author Eunju Jo (301170731)
+ */
+
 import express, {Request, Response, NextFunction} from "express";
 import User from "../models/user";
 
