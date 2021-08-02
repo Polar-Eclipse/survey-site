07931d5e572b113b5537dffd2a801b8bc2ba2168
Jerome Ching <nathan_9_9@hotmail.com>
2021-08-01 21:08:27 -0400

    Added CSV download functionality. Added Fancy Button prototype. Added json2csv dependencies

--- server/util/index.ts
+++ server/util/index.ts
@@ -13,6 +13,7 @@
  */
 
 import { Request, Response, NextFunction } from "express";
+import { Parser } from "json2csv";
 
 /**
  * Redirect the user to the login page if not logged in
@@ -22,4 +23,13 @@ export function authguard(req: Request, res: Response, next: NextFunction): void
         return res.redirect("/login");
     }
     next();
+}
+
+export const downloadResource = (res: Response, filename: string, fields: string[], data: object[]) =>
+{
+    const json2cvs = new Parser ({fields});
+    const cvs = json2cvs.parse(data);
+    res.header('Content-Type', 'text/csv');
+    res.attachment(filename);
+    return res.send(cvs);
 }
