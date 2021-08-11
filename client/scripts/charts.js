49818e7d11ee426cb1097c779a761175f98b9277
eujjo <ejo5@my.centennialcollege.ca>
2021-08-11 02:45:00 -0400

    make the results charts work with multiple-choice questions

--- client/scripts/charts.js
+++ client/scripts/charts.js
@@ -22,17 +22,52 @@
         });
     }
 
-    function Start()
-    {
-        let surveyQuestion = document.getElementsByClassName('chartCanvas');
+    function chart2Draw(option, A, B, C, D){
 
-        for (let i = 1; i <  surveyQuestion.length + 1; i++)
-        {
-            let option = document.getElementById(`question${i}Canvas`).getContext('2d');
-            let qTrue = document.getElementById(`question${i}True`).innerText;
-            let qFalse = document.getElementById(`question${i}False`).innerText;
-            chartDraw(option, qTrue, qFalse);
+        let chart2 = new Chart(option, {
+            type: 'pie',
+            data:{
+                labels:['A','B','C','D'],
+                datasets:[{
+                    label: 'Question 1',
+                    data: [A, B, C, D],
+                    backgroundColor: ['gray', 'black'],
+                    borderWidth: 3,
+                    borderColor: 'green',
+                    hoverBorderWidth: 10,
+                    hoverBorderColor: 'black'
+                }],
+            },
+            options:{}
+        });
+    }
+
+    function Start(){
+        if (document.getElementById(`question${i}True`).innerText != null) {
+            let surveyQuestion = document.getElementsByClassName('chartCanvas');
+
+            for (let i = 1; i <  surveyQuestion.length + 1; i++)
+            {
+                let option = document.getElementById(`question${i}Canvas`).getContext('2d');
+                let qTrue = document.getElementById(`question${i}True`).innerText;
+                let qFalse = document.getElementById(`question${i}False`).innerText;
+                chartDraw(option, qTrue, qFalse);
+            }
         }
+        else {
+            let surveyQuestion = document.getElementsByClassName('chartCanvas');
+    
+            for (let i = 1; i <  surveyQuestion.length + 1; i++)
+            {
+                let option = document.getElementById(`question${i}Canvas`).getContext('2d');
+                let qA = document.getElementById(`question${i}A`).innerText;
+                let qB = document.getElementById(`question${i}B`).innerText;
+                let qC = document.getElementById(`question${i}C`).innerText;
+                let qD = document.getElementById(`question${i}D`).innerText;
+                chart2Draw(option, qA, qB, qC, qD);
+            }
+        }
+
     }
 
     window.addEventListener('load', Start);
